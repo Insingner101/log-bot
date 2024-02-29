395ebd97dec5f2cccced39fabaec98de5afa246d
Athreya Krishna <38380039+Insingner101@users.noreply.github.com>
2024-02-29 21:47:20 +0530

    Version 1.1
    Appends to csv file along with the specified channel

--- bot-2.py
+++ bot-2.py
@@ -91,5 +91,5 @@ async def on_member_update(before, after):
 
             await mod_log_channel.send(log_message)
             break
-
+#Replace without your token
 bot.run('MTIxMjc0MDMzNTU5NjMzOTIwMA.G4w1uD.tmYaSvE0i5VkQ3MShaSemvua32nNmzoYoOCRx0')
